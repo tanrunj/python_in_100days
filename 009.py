9bd2578255f2bfc369399550464cf7da90747efa
tanrunj <309392121@qq.com>
2020-04-15 14:21:15 +0800

    week 3

---
+++ 009.py
@@ -0,0 +1,22 @@
+class Person(object):
+    def __init(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s is playing flying chess.' % self._name)
+        else:
+            print('%s is playing poker.' % self._name)
