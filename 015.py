481ac2fbb86a66a2ddba7702e941cbd9b2729976
tanrunjie <tanrunjie@countrygarden.com.cn>
2020-06-04 16:15:49 +0800

    June 4

---
+++ 015.py
@@ -0,0 +1,216 @@
+prices = {
+    'AAPL': 191.88,
+    'GOOG': 1186.96,
+    'IBM': 149.24,
+    'ORCL': 48.44,
+    'ACN': 166.89,
+    'FB': 208.09,
+    'SYMC': 21.29
+}
+# 用股票价格大于100元的股票构造一个新的字典
+prices2 = {key: value for key, value in prices.items() if value > 100}
+# print(prices2)
+
+"""
+从列表中找出最大的或最小的N个元素
+堆结构(大根堆/小根堆)
+"""
+import heapq
+
+list1 = [34, 25, 12, 99, 87, 63, 58, 78, 88, 92]
+list2 = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+# print(heapq.nlargest(3, list1))
+# print(heapq.nsmallest(3, list1))
+# print(heapq.nlargest(2, list2, key=lambda x: x['price']))
+# print(heapq.nlargest(2, list2, key=lambda x: x['shares']))
+
+"""
+迭代工具模块
+"""
+import itertools
+
+# 产生ABCD的全排列
+itertools.permutations('ABCD')
+# 产生ABCDE的五选三组合
+itertools.combinations('ABCDE', 3)
+# 产生ABCD和123的笛卡尔积
+itertools.product('ABCD', '123')
+# 产生ABC的无限循环序列
+itertools.cycle(('A', 'B', 'C'))
+
+"""
+找出序列中出现次数最多的元素
+"""
+from collections import Counter
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
+    'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes',
+    'look', 'into', 'my', 'eyes', "you're", 'under'
+]
+counter = Counter(words)
+# print(counter.most_common(3))
+
+
+
+def select_sort(items, comp=lambda x, y: x < y):
+    """简单选择排序"""
+    items = items[:]
+    for i in range(len(items) - 1):
+        min_index = i
+        for j in range(i + 1, len(items)):
+            if comp(items[j], items[min_index]):
+                min_index = j
+        items[i], items[min_index] = items[min_index], items[i]
+    return items
+
+def bubble_sort(items, comp=lambda x, y: x > y):
+    """冒泡排序"""
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(0, len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if not swapped:
+            break
+    return items
+
+def bubble_sort2(items, comp=lambda x, y: x > y):
+    """搅拌排序(冒泡排序升级版)"""
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(i, len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if swapped:
+            swapped = False
+            for j in range(len(items) - 2 - i, i, -1):
+                if comp(items[j - 1], items[j]):
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    swapped = True
+        if not swapped:
+            break
+    return items
+
+def merge(items1, items2, comp=lambda x, y: x < y):
+    """合并(将两个有序的列表合并成一个有序的列表)"""
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+        else:
+            items.append(items2[index2])
+            index2 += 1
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+
+def merge_sort(items, comp=lambda x, y: x < y):
+    return _merge_sort(list(items), comp)
+
+
+def _merge_sort(items, comp):
+    """归并排序"""
+    if len(items) < 2:
+        return items
+    mid = len(items) // 2
+    left = _merge_sort(items[:mid], comp)
+    right = _merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+
+
+def seq_search(items, key):
+    """顺序查找"""
+    for index, item in enumerate(items):
+        if item == key:
+            return index
+    return -1
+
+
+def bin_search(items, key):
+    """折半查找"""
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+
+def exhaustion():
+    # 公鸡5元一只 母鸡3元一只 小鸡1元三只
+    # 用100元买100只鸡 问公鸡/母鸡/小鸡各多少只
+    for x in range(20):
+        for y in range(33):
+            z = 100 - x - y
+            if 5 * x + 3 * y + z // 3 == 100 and z % 3 == 0:
+                print(x, y, z)
+
+    # A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
+    # 第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
+    # B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
+    # 然后C、D、E依次醒来也按同样的方式分鱼 问他们至少捕了多少条鱼
+    fish = 6
+    while True:
+        total = fish
+        enough = True
+        for _ in range(5):
+            if (total - 1) % 5 == 0:
+                total = (total - 1) // 5 * 4
+            else:
+                enough = False
+                break
+        if enough:
+            print(fish)
+            break
+        fish += 5
+
+"""
+快速排序 - 选择枢轴对元素进行划分，左边都比枢轴小右边都比枢轴大
+"""
+def __partition(items, start, end, comp):
+    pivot = items[end]
+    i = start - 1
+    for j in range(start, end):
+        if comp(items[j], pivot):
+            i+=1
+            items[i], items[j] = items[j], items[i]
+    items[i+1], items[end] = items[end], items[i+1]
+    return i+1
+
+def _quick_sort(items, start, end, comp):
+    if start < end:
+        pos = __partition(items, start, end, comp)
+        _quick_sort(items, start, pos-1, comp)
+        _quick_sort(items, pos+1, end, comp)
+
+def quick_sort(items, comp=lambda x,y: x<=y):
+    items = list(items)[:]
+    _quick_sort(items, 0 ,len(items)-1, comp)
+    return items
+
+if __name__ == '__main__':
+
+    l1 = [1,3,5,9,7,8,2,4,6]
+    print(quick_sort(l1))
+   
+
+    # print(bin_search(l1,5))
