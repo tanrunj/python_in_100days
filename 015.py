18703e3757dff9e2f56e7bc98d0cf72314c05db8
tanrunjie <tanrunjie@countrygarden.com.cn>
2020-06-05 15:54:53 +0800

    June 5

--- 015.py
+++ 015.py
@@ -207,10 +207,173 @@ def quick_sort(items, comp=lambda x,y: x<=y):
     _quick_sort(items, 0 ,len(items)-1, comp)
     return items
 
+"""
+多个线程竞争一个资源 - 保护临界资源 - 锁（Lock/RLock）
+多个线程竞争多个资源（线程数>资源数） - 信号量（Semaphore）
+多个线程的调度 - 暂停线程执行/唤醒等待中的线程 - Condition
+"""
+from concurrent.futures import ThreadPoolExecutor
+from random import randint
+from time import sleep
+
+import threading
+
+
+class Account():
+    """银行账户"""
+
+    def __init__(self, balance=0):
+        self.balance = balance
+        lock = threading.Lock()
+        self.condition = threading.Condition(lock)
+
+    def withdraw(self, money):
+        """取钱"""
+        with self.condition:
+            while money > self.balance:
+                self.condition.wait()
+            new_balance = self.balance - money
+            sleep(0.001)
+            self.balance = new_balance
+
+    def deposit(self, money):
+        """存钱"""
+        with self.condition:
+            new_balance = self.balance + money
+            sleep(0.001)
+            self.balance = new_balance
+            self.condition.notify_all()
+
+
+def add_money(account):
+    while True:
+        money = randint(5, 10)
+        account.deposit(money)
+        print(threading.current_thread().name, 
+              ':', money, '====>', account.balance)
+        sleep(0.5)
+
+
+def sub_money(account):
+    while True:
+        money = randint(10, 30)
+        account.withdraw(money)
+        print(threading.current_thread().name, 
+              ':', money, '<====', account.balance)
+        sleep(1)
+
+
+def main_thread():
+    account = Account()
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        for _ in range(5):
+            pool.submit(add_money, account)
+            pool.submit(sub_money, account)
+
+
+"""
+多进程和进程池的使用
+多线程因为GIL的存在不能够发挥CPU的多核特性
+对于计算密集型任务应该考虑使用多进程
+time python3 example22.py
+real    0m11.512s
+user    0m39.319s
+sys     0m0.169s
+使用多进程后实际执行时间为11.512秒，而用户时间39.319秒约为实际执行时间的4倍
+这就证明我们的程序通过多进程使用了CPU的多核特性，而且这台计算机配置了4核的CPU
+"""
+import concurrent.futures
+import math
+
+PRIMES = [
+    1116281,
+    1297337,
+    104395303,
+    472882027,
+    533000389,
+    817504243,
+    982451653,
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419
+] * 5
+
+
+def is_prime(n):
+    """判断素数"""
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main_process():
+    """主函数"""
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+
+
+"""
+异步I/O - async / await
+"""
+import asyncio
+
+
+def num_generator(m, n):
+    """指定范围的数字生成器"""
+    yield from range(m, n + 1)
+
+
+async def prime_filter(m, n):
+    """素数过滤器"""
+    primes = []
+    for i in num_generator(m, n):
+        flag = True
+        for j in range(2, int(i ** 0.5 + 1)):
+            if i % j == 0:
+                flag = False
+                break
+        if flag:
+            print('Prime =>', i)
+            primes.append(i)
+
+        await asyncio.sleep(0.001)
+    return tuple(primes)
+
+
+async def square_mapper(m, n):
+    """平方映射器"""
+    squares = []
+    for i in num_generator(m, n):
+        print('Square =>', i * i)
+        squares.append(i * i)
+
+        await asyncio.sleep(0.001)
+    return squares
+
+
+def main_asyncio():
+    """主函数"""
+    loop = asyncio.get_event_loop()
+    future = asyncio.gather(prime_filter(2, 100), square_mapper(1, 100))
+    future.add_done_callback(lambda x: print(x.result()))
+    loop.run_until_complete(future)
+    loop.close()
+
+
 if __name__ == '__main__':
 
     l1 = [1,3,5,9,7,8,2,4,6]
     print(quick_sort(l1))
+    main_asyncio()
    
 
     # print(bin_search(l1,5))
